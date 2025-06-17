c456f2baa56d358c1c472e724b2e1eb86a757a40
itukikikuti <xxvraqwa@gmail.com>
2025-06-17 04:29:46 +0000

    タイトルが出なかったので修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    site: 'https://itukikikuti.github.io/blog/',
-    repo: 'https://github.com/itukikikuti/itukikikuti.github.io',
-    title: '菊池ブログ',
-});
+export default defineConfig({});
